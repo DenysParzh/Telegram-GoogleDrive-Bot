fe1cd6db5a17a95e8fb8d97007480cbc381e3a88
Andrew6932 <vivoto35@gmail.com>
2023-02-09 14:06:48 +0200

    Створення інтерфейсу команди folders_info

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import asyncio
 import logging
 
-from aiogram import types
+from aiogram import types, F
 from aiogram.filters import Command
+from aiogram.filters.callback_data import CallbackData
 from aiogram.fsm.context import FSMContext
 
 import handlers
@@ -10,10 +11,48 @@ from loader import bot, dp
 
 logging.basicConfig(level=logging.INFO)
 
+from loader import service, drive
+from gdrive_utils.scripts import search_file_id
+
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+from aiogram.types import CallbackQuery
+
 
 @dp.message(Command(commands='stop'))
 async def stop_states(message: types.Message, state: FSMContext):
     await state.clear()
+
+
+# class CallBackFolders(CallbackData, prefix="Folder"):
+#     name: str
+
+#
+@dp.message(Command(commands='folders_info'))
+async def folders_info(message: types.Message, folder_id='root'):
+    response = service.files().list(q=f"parents='{folder_id}'").execute()
+    builder = InlineKeyboardBuilder()
+    await message.answer("Download...")
+    i = 0
+    while i < len(response["files"]):
+        response = service.files().list(q=f"parents='{folder_id}'").execute()
+        builder.button(text=response["files"][i]['name'],
+                       callback_data="root")
+        i = i + 1
+
+    builder.button(text='Назад',  callback_data="root")
+
+    builder.adjust(3)
+    await message.answer(
+        "Папки:",
+        reply_markup=builder.as_markup()
+    )
+
+
+@dp.callback_query(CallbackData.filter())
+async def send_random_value(callback: types.CallbackQuery):
+    await callback.message.answer(str(1))
+    await callback.answer()
 
 
 async def main():
