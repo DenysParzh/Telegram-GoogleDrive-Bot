26c62bdeeb62e3e210c66936391b8b5ad26aa80d
Денис Паржницький <Denys.Parzhnyskyi@cs.khpi.edu.ua>
2023-02-13 22:20:08 +0200

    Зміна логіки функції логін

--- handlers/login.py
+++ handlers/login.py
@@ -1,17 +1,21 @@
 from aiogram import types, Router
 from aiogram.filters import Command
 
-from loader import google_auth
+from loader import google_auth, drive
+
 
 router = Router()
+
+AUTH_URL = '<a href ="{}">Vist This Url</a>'
 
 
 @router.message(Command(commands='login'))
 async def user_login(message: types.Message) -> None:
     try:
 
-        google_auth.LocalWebserverAuth()
+        auth_url = google_auth.GetAuthUrl()
+        AUTH = AUTH_URL.format(auth_url)
+        await message.answer(AUTH, parse_mode='HTML')
 
-        await message.answer("Вы залогинились")
     except Exception as ex_info:
         print(f"Інформація про помилку: {ex_info}")
