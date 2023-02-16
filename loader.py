0250929914e6247fd1466d8b685a194396d91f60
Денис Паржницький <Denys.Parzhnyskyi@cs.khpi.edu.ua>
2023-02-16 15:11:16 +0200

    Підключення sql

--- loader.py
+++ loader.py
@@ -1,4 +1,5 @@
 import os
+from dotenv import load_dotenv
 
 from aiogram import Bot, Dispatcher
 from pydrive.auth import GoogleAuth
@@ -8,12 +9,11 @@ from aiogram.fsm.storage.memory import MemoryStorage
 from gdrive.google import create_service
 from utils.constants import CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES
 
+load_dotenv()
 storage = MemoryStorage()
-bot = Bot(token=os.getenv('TOKEN'))
+bot = Bot(token=os.environ["TOKEN"])
 dp = Dispatcher(storage=storage)
 
 google_auth = GoogleAuth()
-#google_auth.LoadCredentialsFile("mycreds.txt")
-#google_auth.SaveCredentialsFile("mycreds.txt")
 drive = GoogleDrive(google_auth)
 service = create_service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
