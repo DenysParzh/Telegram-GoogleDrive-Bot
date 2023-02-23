662c12d9a6c569548cc63c503d9214c9de34c51e
Andrew6932 <vivoto35@gmail.com>
2023-02-23 15:44:11 +0200

    Створення та заповнення файлу config.py. Видалення файлу constants.py

--- utils/constants.py
+++
@@ -1,6 +0,0 @@
-CLIENT_SECRET_FILE: str = 'client_secrets.json'
-API_NAME: str = 'drive'
-API_VERSION: str = 'v3'
-SCOPES = ['https://www.googleapis.com/auth/drive']
-CACHE_FOLDER_NAME: str = "downloaded"
-MIME_TYPE_FOLDER: str = "application/vnd.google-apps.folder"
